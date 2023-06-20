298c897e6110320e337e05551ab25ebd62e97aed
orveodiluca2003 <odiluca@correo.unimet.edu.ve>
2023-06-20 00:12:27 -0400

    Implementacion de lectura de excel

--- BatesHotel/src/bateshotel/BatesHotel.java
+++ BatesHotel/src/bateshotel/BatesHotel.java
@@ -4,6 +4,8 @@
  */
 package bateshotel;
 
+import TXT.LecturaExcel;
+
 /**
  *
  * @author Georgina
@@ -14,7 +16,8 @@ public class BatesHotel {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        LecturaExcel papi = new LecturaExcel();
+        papi.leerfechas();
     }
     
 }
