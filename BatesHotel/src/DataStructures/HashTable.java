d0ea2c9d886b5a6fd7b75d6825c1248f2bbf6784
Juannunes565 <juan.nunes@correo.unimet.edu.ve>
2023-06-21 21:38:48 -0400

    Hashtable hecho, leer excel mejorado

--- BatesHotel/src/DataStructures/HashTable.java
+++ BatesHotel/src/DataStructures/HashTable.java
@@ -4,14 +4,57 @@
  */
 package DataStructures;
 
+import Classes.Client;
+
 /**
  *
- * @author Georgina
+ * @author Juan
  */
 public class HashTable {
-    
     private int size;
+    private List[] table;
+
+    public HashTable(int size) {
+        this.size = size;
+        this.table = new List[size];
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Object[] getTable() {
+        return table;
+    }
+
+    public void setTable(List[] table) {
+        this.table = table;
+    }
     
+    public int hash(String name){
+        double value = 0;
+        for (int x = 0; x < name.length(); x++) {
+            char letter = name.charAt(x);
+            value += (int) letter;
+        }
+        while(Integer.toString((int) value).length() > size || value > size){
+            String valueStr = Integer.toString((int) value);
+            String newStr = valueStr.substring(0, valueStr.length()-1);
+            value = Integer.parseInt(newStr);
+            value = Math.pow(value, (3/2));
+        }
+        
+        return (int) value;
+    }
     
-    
+    public void add(Client client){
+        String name = client.getName();
+        int idx = hash(name);
+        table[idx].append(client);
+    }
+          
 }
--- BatesHotel/src/bateshotel/BatesHotel.java
+++ BatesHotel/src/bateshotel/BatesHotel.java
@@ -5,7 +5,6 @@
 package bateshotel;
 
 import DataStructures.Util;
-import TXT.LecturaExcel;
 
 /**
  *
@@ -17,8 +16,15 @@ public class BatesHotel {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Util u = new Util();
-        u.readDates();
+//        Util u = new Util();
+//        u.readDates();
+        int num = (int) (Math.random()*2+1);
+        if(num == 1){
+            System.out.println("Lo hace Juan");
+        }
+        else{
+            System.out.println("Lo hace este marico");
+        }
     }
     
 }
